9d64fad2fc4df22b4bdac6e0eff10034fe44ae6d
LegoWorksStudent <122985953+legoworksstudents@users.noreply.github.com>
2024-12-04 18:35:27 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    radio.sendNumber(2)
+    radio.sendNumber(4)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-    radio.sendNumber(4)
+    radio.sendNumber(2)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendNumber(1)
@@ -10,9 +10,6 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     radio.sendNumber(3)
 })
-basic.showNumber(4)
-radio.setGroup(4)
+basic.showNumber(1)
+radio.setGroup(1)
 joystickbit.initJoystickBit()
-basic.forever(function () {
-	
-})
